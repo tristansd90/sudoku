367e48c3d37f4bef68cd40d957f9d414f7794e3a
Tristan SCHMID <tristan.90@icloud.com>
2022-02-21 15:14:29 +0100

    insert value in array ok

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ const plateau = document.querySelector("#plateau");
 const inputs = document.querySelectorAll("input");
 let hasWon = false;
 
+
 const sudoku = [
     [1, 0, 6, 0, 3, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 8, 2, 0, 0],
@@ -14,7 +15,8 @@ const sudoku = [
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
 ];
 
-// recuperer les valeurs du tableau js et les placer dans la table html
+// recuperer les valeurs du tableau js et les placer dans la table html.
+// remplacer les 0 par des numeros valides
 sudoku.forEach((row, numRow) => {
     row.forEach((column, numColumn) => {
         if (column == 0) {
@@ -23,24 +25,43 @@ sudoku.forEach((row, numRow) => {
             plateau.children[numRow].children[numColumn].firstChild.value = column;
             plateau.children[numRow].children[numColumn].firstChild.disabled = true;
         }
+
+        plateau.children[numRow].children[numColumn].firstChild.addEventListener("keyup", (e) => {
+            if (e.key === "Enter") {
+                sudoku[numRow][numColumn] = parseInt(plateau.children[numRow].children[numColumn].firstChild.value);
+                console.log(sudoku);
+            }
+        })
     });
 });
 
-//verifier les valeurs entre 0 et 9
+//verifier les valeurs entre 0 et 9, et si valide les ajouter au tableau suddoku
 function checkValue() {
-for (let i=0; i<inputs.length; i++)
-    inputs[i].addEventListener("keyup", () => {
-        if (inputs[i].value < 1 || inputs[i].value > 9) {
-            console.log("error value")
-            inputs[i].classList.add("error_value");
-        } else {
-            inputs[i].classList.remove("error_value");
-        }
-    })
+    for (let i = 0; i < inputs.length; i++) {
+        inputs[i].addEventListener("keyup", () => {
+            if (inputs[i].value < 1 || inputs[i].value > 9) {
+                inputs[i].classList.add("error_value");
+            } else {
+                inputs[i].classList.remove("error_value");
+            };
+        })
+    }
 };
-checkValue()
+checkValue();
+
+// function addValue() {
+//     sudoku.forEach((row, numRow) => {
+//         row.forEach((column, numColumn) => {
 
 
-// verifer les doublons
 
-
+//             inputs.addEventListener("keyup", (e) => {
+//                 if (e.key === "Enter") { 
+//                     sudoku[numRow][numColumn] = parseInt(column.value);
+//                     console.log(sudoku);
+//                 }
+//             })
+//         });
+//     });
+// };
+// addValue();
